3924e529d3b39477991226965753fefdd49b7b81
SiraprapaLaokaen <Siraprapa.laok@bumail.net>
2025-03-22 12:16:25 +0700

    edit alien script

--- Assets/script/AlienScript.cs
+++ Assets/script/AlienScript.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class AlienScript : MonoBehaviour
 {
     public GameObject Target;
+   /* public Transform player;
+    public float speed = 3f;
+    private Rigidbody rb; */
+
     public Transform player;
     public float speed = 3f;
+    public float detectionRange = 10f; 
     private Rigidbody rb;
  
     void Start()
@@ -20,9 +25,22 @@ public class AlienScript : MonoBehaviour
 
     }
  
-    void FixedUpdate()
+    /*void FixedUpdate()
     {
         Vector3 direction = (player.position - transform.position).normalized;
         rb.MovePosition(transform.position + direction * speed * Time.fixedDeltaTime);
+    }*/
+
+    void FixedUpdate()
+    {
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        if (distance <= detectionRange) // ตรวจสอบระยะ
+        {
+            Vector3 direction = (player.position - transform.position).normalized;
+            direction.y = 0; // ป้องกันศัตรูลอยขึ้น
+
+            rb.MovePosition(transform.position + direction * speed * Time.fixedDeltaTime);
+        }
     }
 }
