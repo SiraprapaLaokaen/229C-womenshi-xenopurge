881a142b68c5202669d3498aeb5f7643c08032e9
SiraprapaLaokaen <Siraprapa.laok@bumail.net>
2025-03-24 13:25:10 +0700

    edit shooter script

--- Assets/script/Shooter.cs
+++ Assets/script/Shooter.cs
@@ -23,25 +23,25 @@ public class Shooter : MonoBehaviour
         {
             Debug.DrawRay( Bullet.position, transform.forward * 30, Color.red);
 
-            if ( Input.GetMouseButtonDown(0) )
+            if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1)) // ตรวจจับคลิกซ้ายหรือขวา 
             {
- 
-                Instantiate( vfxHitpoint , hit.point , Quaternion.identity );
+                Instantiate(vfxHitpoint, hit.point, Quaternion.identity);
 
-                if ( hit.collider.name == "alien" || hit.collider.name == "alien1" || hit.collider.name == "alien2" || hit.collider.name == "alien3" 
-                || hit.collider.name == "alien4" || hit.collider.name == "alien5" || hit.collider.name == "alien6" || hit.collider.name == "alien7" 
-                || hit.collider.name == "alien8" || hit.collider.name == "alien9" || hit.collider.name == "alien10" || hit.collider.name == "alien11" || hit.collider.name == "alienboss" )
-                {
-                    Enemy alien = hit.collider.GetComponent< Enemy >();
- 
-                    if ( alien != null )
+               if (hit.collider.name == "alien" || hit.collider.name == "alien1" || hit.collider.name == "alien2" || hit.collider.name == "alien3" 
+                  || hit.collider.name == "alien4" || hit.collider.name == "alien5" || hit.collider.name == "alien6" || hit.collider.name == "alien7" 
+                  || hit.collider.name == "alien8" || hit.collider.name == "alien9" || hit.collider.name == "alien10" || hit.collider.name == "alien11" 
+                  || hit.collider.name == "alienboss")
+                  {
+                    Enemy alien = hit.collider.GetComponent<Enemy>();
+
+                    if (alien != null)
                     {
-                        alien.TakeDamage(2);
+                        int damage = Input.GetMouseButtonDown(0) ? 2 : 3; // คลิกซ้าย = 2 | คลิกขวา = 3
+                        alien.TakeDamage(damage);
                     }
- 
-                }
 
-                
+        
+                  }
             }
         }
         
